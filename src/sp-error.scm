30f73b5c8bfbc23db050d1088774ec69f317db75
niyarin <inoanet@gmail.com>
2019-08-30 20:05:03 +0900

    Change the color of error discription.

--- src/sp-error.scm
+++ src/sp-error.scm
@@ -2,12 +2,21 @@
 
 (define-library (silver-paren error)
    (import (scheme base)
+           (scheme write)
            (niyarin rainbow write))
-   (export silver-paren-error-mes)
+   (export silver-paren-error-mes
+           silver-paren-error)
    (begin
      
      (define (silver-paren-error-mes mes . opt-port)
        (let ((port (if (null? opt-port) (current-error-port) (car opt-port))))
-         (display-first-color mes port)
+         (display-first-color 
+              "Silver-paren error: "
+           port)
+         (display 
+           mes
+           port)
          ))
+     (define (silver-paren-error error-message . opt)
+       (error error-message opt))
      ))
