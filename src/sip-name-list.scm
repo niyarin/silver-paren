4c9dea70b6eac9dc34cb6902621a485136475b64
niyarin <inoanet@gmail.com>
2019-09-12 10:22:19 +0900

    Add name-list generator from command-line options

--- src/sip-name-list.scm
+++ src/sip-name-list.scm
@@ -1,10 +1,17 @@
+(include "./sip-error.scm")
+
 (define-library (silver-paren name-list)
    (import 
      (scheme base)
+     (scheme write)
      (scheme cxr)
      (only (srfi 1) fold);scheme list
+     (silver-paren error)
      )
-   (export silver-paren-name-list-valid-name-list?)
+   (export silver-paren-name-list-valid-name-list?
+           silver-paren-name-list-name 
+           silver-paren-name-list-generate-name-list-list-from-option 
+           )
 
    (begin
      ;example (SOFTNAME VERSION? OPT? ...)
@@ -40,4 +47,59 @@
                 (cdr name-list)))))
          (else 
            #f)))
-   ))
+
+    (define (silver-paren-name-list-name name-list)
+      (car name-list))
+
+    (define (%edition-delimiter? object edition-delimiter)
+      (and
+        (eq? object edition-delimiter )))
+
+    (define (silver-paren-name-list-generate-name-list-list-from-option 
+              option edition-delimiter)
+         (let loop ((current-package-name '())
+                    (current-package-modifications '())
+                    (option option)
+                    (res '()))
+           (cond 
+             ((and (null? option) (null? current-package-name)) (reverse res))
+             ((null? option)
+              (reverse
+                (cons 
+                  (cons current-package-name
+                        current-package-modifications)
+                  res)))
+             ((equal? (car option) edition-delimiter)
+               (when (null? (cdr option))
+                  (silver-paren-error  
+                    (string-append 
+                      "Edition delimiter "
+                      (symbol->string edition-delimiter)
+                      "requires argument.")))
+               (loop
+                 current-package-name
+                 (cons 
+                   (list
+                     'edition
+                     (cadr option))
+                   current-package-modifications)
+                 (cddr option)
+                 res))
+
+             ((null? current-package-name)
+              (loop 
+                (car option)
+                '()
+                (cdr option)
+                res))
+             (else
+               (loop
+                 (car option)
+                 '()
+                 (cdr option)
+                 (cons 
+                   (cons
+                     current-package-name
+                     current-package-modifications)
+                   res))))))
+    ))
--- test/test-sip-name-list.scm
+++ test/test-sip-name-list.scm
@@ -5,12 +5,11 @@
    (import 
         (scheme base)
         (srfi 78) ;Lightweight testing
-        (silver-paren name-list)
-        )
+        (silver-paren name-list))
 
    (begin
 
-      (define (internal-valid-name-list?-test)
+      (define (%valid-name-list?-test)
          (check (silver-paren-name-list-valid-name-list? '("PIYO")) => #t)
          (check (silver-paren-name-list-valid-name-list? '()) => #f)
          (check (silver-paren-name-list-valid-name-list? '(3)) => #f)
@@ -20,6 +19,14 @@
          (check (silver-paren-name-list-valid-name-list? '("PIYO" (version 'abc))) => #f)
         )
 
+      (define (%silver-paren-name-list-generate-name-list-list-from-option-test)
+         (check (silver-paren-name-list-generate-name-list-list-from-option '("piyo" ":edition" "foo") ":edition")
+            =>  '(("piyo" (edition "foo"))))
+         (check (silver-paren-name-list-generate-name-list-list-from-option '("piyo" ":edition" "foo" "boo" "var") ":edition")
+            =>  '(("piyo" (edition "foo"))("boo") ("var")))
+         )
+
       (define (test-silver-paren-name-list)
-         (internal-valid-name-list?-test)
+         (%valid-name-list?-test)
+         (%silver-paren-name-list-generate-name-list-list-from-option-test)
         )))
