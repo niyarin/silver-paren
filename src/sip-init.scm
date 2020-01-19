c1804ad0d83e9cc8cd93dca19cb4a8de70c53e4a
niyarin <inoanet@gmail.com>
2020-01-19 23:30:54 +0900

    Add support for create imprementation directories.

--- src/sip-init.scm
+++ src/sip-init.scm
@@ -6,12 +6,13 @@
            (scheme file)
            (scheme write);debug
            (silver-paren misc)
-           (silver-paren error))
+           (silver-paren error)
+           (silver-paren log))
    (export silver-paren-init/inited? silver-paren-init/init)
    (begin
       (define (silver-paren-init/inited? silver-paren-directory-path)
         (file-exists? 
-          (silver-paren-misc-concatenate-path
+          (silver-paren-misc/concatenate-path
             silver-paren-directory-path
             "config.list")))
 
@@ -19,10 +20,15 @@
         (let* ((target-dir
                 (cond ((assv 'silver-paren-directory-path config) => cadr)))
                (outsourcing-flag
-                (cond ((assv 'silver-paren-directory-outsourcing config) => cadr )))
+                (cond ((assv 'silver-paren-directory-outsourcing config)
+                       => cadr)))
                (_restart-symbol
-                 (cond ((assv 'silver-paren-restart-symbol config) => cadr )
+                 (cond ((assv 'silver-paren-restart-symbol config) => cadr)
                        (else  '_)))
+               (imprementation-name
+                 (cond ((assv 'implementation config)
+                        => cadr)
+                       (else "default")))
                (restart-symbol
                  (if (eq? _restart-symbol '_)
                    'silver-paren-init:create-sip-dir
@@ -33,11 +39,23 @@
                restart-symbol
                (silver-paren-init:create-sip-dir
                   (silver-paren-misc/create-dir target-dir outsourcing-flag)
+                  (silver-paren-log-message
+                    (string-append "(mkdir " target-dir ")"))
                   (silver-paren-misc/exit-with-restart-simple
                     'silver-paren-init:create-sip-current-imprementation-dir
-                    outsourcing-flag
-                    ))
+                    outsourcing-flag))
                (silver-paren-init:create-sip-current-imprementation-dir
-                 (display "RESTART OK")(newline))))
+                 (let ((imp-dir
+                         (silver-paren-misc/concatenate-path
+                                 target-dir
+                                 imprementation-name)))
+                    (silver-paren-misc/create-dir imp-dir outsourcing-flag)
+                    (silver-paren-log-message
+                       (string-append "(mkdir " imp-dir ")")))
+                 (silver-paren-misc/exit-with-restart-simple
+                   'silver-paren-init:create-current-imprementation-config-file
+                   outsourcing-flag))
+               (silver-paren-init:create-current-imprementation-config-file
+                 (display "HELLO")(newline))))
             (else
               ))))))
